7bac9fe88f4887b0db6d09d9cab2c0afee966d4f
juanestrella2098 <131753015+juanestrella2098@users.noreply.github.com>
2024-09-26 08:55:46 +0200

    Nuevos ejercicios
    Ejercicios 2.1 movidos a la carpeta 2.1 y creada la carpeta 2.2 con sus ejercicios correspondientes

---
+++ TEMA 2/Juan2.2/Juan2.2.2/Juan2.2.2.js
@@ -0,0 +1,15 @@
+/* Utiliza la consola para escribir una expresión que compare dos strings y produzca un valor TRUE */
+
+console.log("a" == "a");
+
+/* Escribí una expresión comparando dos strings que devuelva FALSE. */
+
+console.log("a" == "b");
+
+/* Escribí una expresión que involucre un string y un número y devuelva FALSE. */
+
+console.log("7" === 7);
+
+/* Escribí una expresión que involucre un string y un número y devuelva TRUE */
+
+console.log("7" == 7);
