5a9237945330d584f9035b124900cda0a1461633
juanestrella2098 <131753015+juanestrella2098@users.noreply.github.com>
2024-10-15 10:18:53 +0200

    Modificado 2.10.11
    Modificado el ejercicio 2.10.11 para que sea aunque mas largo algo mas entendible

--- TEMA 2/Juan2.10/Juan2.10.11/Juan2.10.11.js
+++ TEMA 2/Juan2.10/Juan2.10.11/Juan2.10.11.js
@@ -5,16 +5,40 @@ const arrObjs = [
         nombre: "a"
     },
     {
-        id: 1,
+        id: 12,
         nombre: "pe"
     },
     {
-        id: 12,
+        id: 1,
         nombre: "la"
     },
+    {
+        id:3,
+        nombre:"pera"
+    },
 ]
+//ordenamos por id
+arrObjs.sort((a, b) => a.id - b.id);
 
-//Mapeamos cada objeto del array con obj.id como clave y convertmos el Map de nuevo usando Array.from y obtenemos solo los valores unicos
-const mapObj = Array.from(new Map(arrObjs.map(obj => [obj.id, obj.nombre])).values())
+//Extraemos las id del array de objetos con map
+const objKeys = arrObjs.map(obj => obj.id);
 
-console.log(mapObj);
+//Eliminamos los duplicados
+const arrKeysPuro = [...new Set(objKeys)];
+
+//Creamos un array vacios donde se copiaran los objetos
+const arrObjsSinDupli = [];
+
+//Creamos una variable id que empieza siempre en -1 para que entre en la primera interacción
+let id = -1;
+
+//Recorremos el array
+arrObjs.forEach((obj) => {
+    //Si el id del objeto no es el mismo del que se asignara debajo agregamos el objeto
+    if(id!=obj.id){
+        arrObjsSinDupli.push(obj);
+    }
+    id=obj.id;
+})
+
+console.log(arrObjsSinDupli);
