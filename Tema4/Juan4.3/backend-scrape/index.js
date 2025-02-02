c7c56deea97b991043be6060df89d229f60b8bf6
juanestrella2098 <131753015+juanestrella2098@users.noreply.github.com>
2025-02-02 20:39:35 +0100

    Aplicacion scraping
    Añadida aplicacion de scraping

---
+++ Tema4/Juan4.3/backend-scrape/index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const cors = require('cors')
+const puppeteer = require('puppeteer');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(cors())
+
+// Escrapear en amazon
+async function scrapeAmazon(url) {
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const titlePage = await page.$('#title #productTitle');
+    let title = await page.evaluate(el => el.textContent, titlePage);
+    title = title.trim();
+
+    const pricePage = await page.$('#priceblock_ourprice, #priceblock_dealprice, #priceblock_saleprice, .a-price .a-offscreen');
+    const price = await page.evaluate(el => el ? el.textContent.trim() : 'Precio no disponible', pricePage);
+
+    const imagePage = await page.$('.imgTagWrapper img');
+    const image = await page.evaluate(el => el.src, imagePage);
+
+    await browser.close();
+
+    return { title, price, image };
+}
+
+//Escrapear en bing para poder sacar varios precios y sus paginas
+async function scrapeBing(title) {
+
+
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    title = title.split(' ').slice(0, 3).join('-');
+    const page = await browser.newPage();
+
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36');
+
+    await page.goto(`https://www.bing.com/shop?q=${title}`);
+    await page.waitForSelector('#br-paidOffersGrid');
+    const results = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('li.br-item')).map((list, index) => ({
+            position: index + 1,
+            price: list.querySelector('.pd-price') ? list.querySelector('.pd-price').textContent.trim() : null,
+            venta: list.querySelector('.br-sellersCite') ? list.querySelector('.br-sellersCite').textContent.trim() : null,
+            compareLink: list.querySelector('a.br-compareSellers') ? list.querySelector('a.br-compareSellers').href : null
+        }));
+    });
+
+    const products = results.slice(0, 5);
+
+    await browser.close();
+
+    return products;
+}
+
+//Ruta para hacer scraping
+app.get('/scrape', async (req, res) => {
+    const url = req.query.url;
+
+    if (!url) {
+        return res.status(404).json({
+            error: "La url es obligatoria"
+        });
+    }
+
+    const amazonData = await scrapeAmazon(url);
+    const bingData = await scrapeBing(amazonData.title);
+
+    res.json({ amazonData, bingData });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
---
+++ Tema4/Juan4.3/web-scrap/src/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+  $("#buscarBtn").on('click', function () {
+    const url = $('#buscar').val();
+    if (url) {
+      $.ajax({
+        url: `http://localhost:3000/scrape?url=${encodeURIComponent(url)}`,
+        method: 'GET',
+        success: function (data) {
+          const amazonData = data.amazonData;
+          const amazonHtml = `
+              <div class="producto">
+                <h2>${amazonData.title.replace(/\+/g, ' ')}</h2>
+                <img src="${amazonData.image}" alt="${amazonData.title}">
+                <p>Precio: ${amazonData.price ? amazonData.price : 'Precio no disponible'}</p>
+                <a href="${url}" target="_blank">Ver en Amazon</a>
+              </div>`;
+          $('#resultadoAmazon').html(amazonHtml);
+
+          const bingData = data.bingData;
+          let bingHTML = '';
+          bingData.forEach(product => {
+            bingHTML += `
+                <div class="producto">
+                  <p>Posición: ${product.position}</p>
+                  <p>Precio: ${product.price ? product.price : 'Precio no disponible'}</p>
+                  <p>Venta: ${product.venta}</p>
+                  <a href="${product.compareLink}" target="_blank">Ver el precio del vendedor</a>
+                </div>
+              `;
+          });
+          $('#resultadosBing').html(bingHTML);
+        },
+        error: function (error) {
+          console.error('Error en la petición:', error);
+        }
+      });
+    } else {
+      alert('Por favor, ingresa una URL');
+    }
+  });
+
+  $("#buscar").on('keydown', function (event) {
+    if (event.key === 'Enter') {
+      $("#buscarBtn").click();
+    }
+  });
+});
